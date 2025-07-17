7ac08bac841f5a6a68d94f606136d2363a04bc01
Raul Alberto Pacheco Rodriguez <raulavenger21@gmail.com>
2025-07-17 21:14:44 +0000

    aun no se resuelve

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv';
+config(); // Carga las variables de entorno del archivo .env
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,3 @@
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/generate-schema-suggestion.ts';
 import '@/ai/flows/generate-summary.ts';
 import '@/ai/flows/writing-assistant.ts';
